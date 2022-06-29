5fc5d72c1e8ce223cef804fd45bf7c2d3d68d18d
Juan Vasquez <juan.vasquez@unir.net>
2022-06-29 16:48:04 +0200

    ejercicio 1

---
+++ 01 ts-intro/src/ejercicios/01-tipos-basicos.ts
@@ -0,0 +1,11 @@
+/*
+    ===== Código de TypeScript =====
+*/
+
+let personaje: string = "Thor";
+let fuerza: number | string = 100;
+let habilitado: boolean = true;
+
+fuerza = 'FULL';
+
+console.log(personaje,fuerza,habilitado);

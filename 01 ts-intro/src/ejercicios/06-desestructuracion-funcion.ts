192c08f103ff9e32c9aeb4b7ddcc845eae640a88
Juan Vasquez <juan.vasquez@unir.net>
2022-06-30 17:16:06 +0200

    desestructuración en funciones

---
+++ 01 ts-intro/src/ejercicios/06-desestructuracion-funcion.ts
@@ -0,0 +1,28 @@
+interface Producto{
+    desc: string;
+    precio: number;
+}
+
+const tableta: Producto = {
+    desc:'Tablet',
+    precio: 50
+}
+const telefono: Producto = {
+    desc: 'Samsung x5',
+    precio: 50
+}
+
+function CalcularISV(productos: Producto[]): [number, number]{
+    let total = 0;
+
+    productos.forEach(({precio}) =>{
+        total += precio;
+    });
+
+    return [total, total * 0.15];
+}
+
+const productosISV = [tableta,telefono];
+const [total, isv] = CalcularISV(productosISV);
+console.log('total:',total);
+console.log('isv:',isv);
